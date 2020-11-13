a0bfa110689ef387b7cf86a5092ce5395e7982c6
Kayan-dev <kaya-development@outlook.com>
2020-11-13 13:22:35 +0100

    new frontend version

---
+++ public/precache-manifest.c89ec5ea7fa9c60f253da1a6580cdf91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ae6625a16fba387d25049eed21f4db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9e55df158c85acb9bdf",
+    "url": "/static/css/2.3b5e5dde.chunk.css"
+  },
+  {
+    "revision": "2b31a7dddad31b491f6d",
+    "url": "/static/css/main.0f8286b8.chunk.css"
+  },
+  {
+    "revision": "e9e55df158c85acb9bdf",
+    "url": "/static/js/2.1b7f2135.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/static/js/2.1b7f2135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b31a7dddad31b491f6d",
+    "url": "/static/js/main.d34ca0e6.chunk.js"
+  },
+  {
+    "revision": "2180c58ea2d817ef59ff",
+    "url": "/static/js/runtime-main.baec2ef2.js"
+  }
+]);
--- public/service-worker 2.js
+++ public/service-worker 2.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c89ec5ea7fa9c60f253da1a6580cdf91.js"
+  "/precache-manifest.608c48f0b431d075abb97654ce83e8a9.js"
 );
 
 self.addEventListener('message', (event) => {
