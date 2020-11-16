4890baf207fb04aabae220a61707391035341206
Kayan-dev <kaya-development@outlook.com>
2020-11-16 17:34:04 +0100

    a try to fix performance

--- index.js
+++ index.js
@@ -83,11 +83,11 @@ app.use(corsMiddleWare());
  * - the delay time can be configured in the package.json
  */
 
-if (process.env.DELAY) {
-  app.use((req, res, next) => {
-    setTimeout(() => next(), parseInt(process.env.DELAY));
-  });
-}
+// if (process.env.DELAY) {
+//   app.use((req, res, next) => {
+//     setTimeout(() => next(), parseInt(process.env.DELAY));
+//   });
+// }
 
 /**
  *
